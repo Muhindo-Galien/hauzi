4df5235a64f1845e0d0c6e62d4046ef05ed99380
Muhindo-Galien <muhindogalien512@gmail.com>
2023-04-16 02:37:21 +0300

    Hauzi base setup

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Hauzi</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Hauzi Real-Eastate" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import { Nunito } from '@next/font/google'
+import "./globals.css"
+
+const font = Nunito({ subsets: ['latin'] })
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body className={font.className}>{children}</body>
+    </html>
+  )
+}
