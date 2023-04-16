d79259ce9a48d7ee37ef495551471505af3e0e02
Muhindo-Galien <muhindogalien512@gmail.com>
2023-04-16 04:31:07 +0300

    NavBar added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,24 @@
-import { Nunito } from '@next/font/google'
-import "./globals.css"
+import { Nunito } from '@next/font/google';
+import './globals.css';
+import NavBar from './components/navbar/NavBar';
+import ClientOnly from './components/ClientOnly';
 
-const font = Nunito({ subsets: ['latin'] })
+const font = Nunito({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang='en'>
       <head />
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <ClientOnly>
+          <NavBar />
+        </ClientOnly>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
